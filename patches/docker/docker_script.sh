bb80a5ec0974a8fda60be66466cbae6392620047
Marc van Hoof <marcvanhoof@gmail.com>
2023-07-05 13:22:59 +0200

    Revert "update files"
    This reverts commit e33b70134d84227dffafaf76208f87a27ca9ad0e.

--- patches/docker/docker_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo echo 'conda activate vitis-ai-pytorch' >> /etc/banner.sh
-sudo echo './compile.py --name tf2_resnet50_imagenet_224_224_7.76G_2.5 --arch arch.json' >> /etc/banner.sh
-sudo echo 'exit' >> /etc/banner.sh
-bash
